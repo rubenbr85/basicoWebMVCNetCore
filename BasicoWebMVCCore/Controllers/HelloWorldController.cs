de0865c0c72902689371bf2cca0c791f212b9815
Ruben Berreguero <rubenbr85@gmail.com>
2020-02-02 18:47:58 +0100

    Creacion de vista para la accion Welcome: se usó el diccionario ViewData para pasar datos del controlador a una vista

--- BasicoWebMVCCore/Controllers/HelloWorldController.cs
+++ BasicoWebMVCCore/Controllers/HelloWorldController.cs
@@ -20,9 +20,12 @@ namespace BasicoWebMVCCore.Controllers
         // 
         // GET: /HelloWorld/Welcome/ 
 
-        public string Welcome(string name, int numTimes = 1)
+        public IActionResult Welcome(string name, int numTimes = 1)
         {
-            return HtmlEncoder.Default.Encode($"Hello {name}, NumTimes is: {numTimes}");
+            ViewData["Message"] = "Hello " + name;
+            ViewData["NumTimes"] = numTimes;
+
+            return View();
         }
     }
 }
