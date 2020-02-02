6a711191e67d801ec0c316f92bcd08abaf77dd39
Ruben Berreguero <rubenbr85@gmail.com>
2020-02-02 19:46:46 +0100

    MIgracion de base de datos Rating

--- BasicoWebMVCCore/Migrations/20200202184418_Rating.Designer.cs
+++ BasicoWebMVCCore/Migrations/20200202184418_Rating.Designer.cs
@@ -4,14 +4,16 @@ using BasicoWebMVCCore.Data;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
 using Microsoft.EntityFrameworkCore.Metadata;
+using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 
 namespace BasicoWebMVCCore.Migrations
 {
     [DbContext(typeof(BasicoWebMVCCoreContext))]
-    partial class BasicoWebMVCCoreContextModelSnapshot : ModelSnapshot
+    [Migration("20200202184418_Rating")]
+    partial class Rating
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder
