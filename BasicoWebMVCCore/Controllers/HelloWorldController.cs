ef237dc6107860c106e1db0d46a6a1b0da43a889
Ruben Berreguero <rubenbr85@gmail.com>
2020-02-02 18:38:59 +0100

    Crear vista para la accion Index de HelloWordController

--- BasicoWebMVCCore/Controllers/HelloWorldController.cs
+++ BasicoWebMVCCore/Controllers/HelloWorldController.cs
@@ -12,9 +12,9 @@ namespace BasicoWebMVCCore.Controllers
         // 
         // GET: /HelloWorld/
 
-        public string Index()
+        public IActionResult Index()
         {
-            return "This is my default action...";
+            return View();
         }
 
         // 
