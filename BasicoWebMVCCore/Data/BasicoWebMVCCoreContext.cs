a694977760594d4b17d6fa4659fcc0b56939a32a
Ruben Berreguero <rubenbr85@gmail.com>
2020-02-02 19:01:44 +0100

    Crear controlador MoviesController y sus vistas con scafolding

---
+++ BasicoWebMVCCore/Data/BasicoWebMVCCoreContext.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.EntityFrameworkCore;
+using BasicoWebMVCCore.Models;
+
+namespace BasicoWebMVCCore.Data
+{
+    public class BasicoWebMVCCoreContext : DbContext
+    {
+        public BasicoWebMVCCoreContext (DbContextOptions<BasicoWebMVCCoreContext> options)
+            : base(options)
+        {
+        }
+
+        public DbSet<BasicoWebMVCCore.Models.Movie> Movie { get; set; }
+    }
+}
--- BasicoWebMVCCore/Startup.cs
+++ BasicoWebMVCCore/Startup.cs
@@ -1,4 +1,4 @@
-using System;
+﻿using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
@@ -8,6 +8,8 @@ using Microsoft.AspNetCore.HttpsPolicy;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
+using Microsoft.EntityFrameworkCore;
+using BasicoWebMVCCore.Data;
 
 namespace BasicoWebMVCCore
 {
@@ -24,6 +26,9 @@ namespace BasicoWebMVCCore
         public void ConfigureServices(IServiceCollection services)
         {
             services.AddControllersWithViews();
+
+            services.AddDbContext<BasicoWebMVCCoreContext>(options =>
+                    options.UseSqlServer(Configuration.GetConnectionString("BasicoWebMVCCoreContext")));
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
